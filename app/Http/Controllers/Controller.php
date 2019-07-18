60c3e29ecbdda90d7018ca9ac7a12366a67ae793
Guilherme Silva <guilhermefos.developer@gmail.com>
2019-07-18 11:46:27 -0300

    Add Swagger and Passport Packages

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,33 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * @OA\Info(
+ *
+ *      version="1.0.0",
+ *      title="VEHICLE API",
+ *      description="Vehicle API",
+ *      @OA\Contact(
+ *          email="guilhermefos.developer@gmail.com"
+ *      ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ *  * @OA\SecurityScheme(
+ *     type="apiKey",
+ *     in="header",
+ *     securityScheme="api_key",
+ *     name="Authorization",
+ * )
+ *
+ * @OA\Server(
+ *      url="/api/v1/",
+ *      description="Localhost"
+ * )
+ *
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
