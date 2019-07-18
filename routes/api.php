91837d1973870d343e6fb2057750262dcb352ba2
Guilherme Silva <guilhermefos.developer@gmail.com>
2019-07-18 11:41:09 -0300

    Create API routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,22 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1'], function () {
+
+    Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function () {
+        Route::post('register', ['as' => 'register', 'uses' => 'RegisterController@register']);
+        Route::post('login', ['as' => 'login', 'uses' => 'LoginController@login']);
+        Route::get('logout', ['as' => 'logout', 'uses' => 'LoginController@logout'])->middleware(['auth:api']);
+    });
+
+    Route::group(['middleware' => 'auth:api'], function () {
+
+        Route::group(['prefix' => 'vehicle', 'middleware' => 'scope:self'], function () {
+            Route::get('/', ['as' => 'vehicle.index', 'uses' => 'VehicleController@index']);
+            Route::post('/store', ['as' => 'vehicle.store', 'uses' => 'VehicleController@store']);
+            Route::put('/update', ['as' => 'vehicle.update', 'uses' => 'VehicleController@update']);
+            Route::delete('/destroy/{id}', ['as' => 'vehicle.destroy', 'uses' => 'VehicleController@destroy']);
+        });
+
+    });
 });
