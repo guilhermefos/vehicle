18cbd112cb399770200e2f5a36025cd3b8258da1
Guilherme Silva <guilhermefos.developer@gmail.com>
2019-07-18 11:38:13 -0300

    Create Migrations

---
+++ database/migrations/2019_07_16_154408_create_vehicles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('brand');
+            $table->string('chassis')->unique()->index();
+            $table->string('color');
+            $table->string('model');
+            $table->dateTime('year');
+            $table->string('plate');
+            $table->string('renavam');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
